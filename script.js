de701a454a93e0aac8d4e6c082e03b0c4209a475
kapilbalodi <kapilbalodi81@gmail.com>
2026-02-11 11:22:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showPromise() {
+  const promise = document.getElementById("promiseText");
+  promise.innerHTML =
+    "Annabella ❤️ I promise to always respect you, support you, protect your smile, and stand by you no matter what. You are very special to me 💖";
+  promise.classList.add("show");
+}
