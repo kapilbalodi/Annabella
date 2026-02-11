6cda65e268dbaa90334721c0853659fe71a49b02
kapilbalodi <kapilbalodi81@gmail.com>
2026-02-11 11:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function showPromise() {
-  const promise = document.getElementById("promiseText");
-  promise.innerHTML =
-    "Annabella ❤️ I promise to always respect you, support you, protect your smile, and stand by you no matter what. You are very special to me 💖";
-  promise.classList.add("show");
+const text = `Happy Promise Day Annabella ❤️
+
+I promise to always respect you,
+support you in every situation,
+protect your smile,
+and stand by you no matter what.
+
+You are very special to me 💖`;
+
+let index = 0;
+let typingInterval;
+
+function startTyping() {
+  const typingText = document.getElementById("typingText");
+  const sound = document.getElementById("typingSound");
+
+  typingText.innerHTML = "";
+  index = 0;
+
+  sound.loop = true;
+  sound.play();
+
+  typingInterval = setInterval(() => {
+    if (index < text.length) {
+      typingText.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(typingInterval);
+      sound.pause();
+      sound.currentTime = 0;
+    }
+  }, 50); // typing speed
 }
